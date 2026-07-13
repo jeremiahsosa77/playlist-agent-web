b09b647dbc53aa9580ee3d2e66e992d98230e329
jeremiahsosa77 <jeremiahsosa27@gmail.com>
2026-07-13 01:23:22 -0500

    allow Spotify album artwork in Next images

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,13 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "i.scdn.co",
+        pathname: "/image/**",
+      },
+      {
+        protocol: "https",
         hostname: "images.unsplash.com",
+        pathname: "/**",
       },
     ],
   },
